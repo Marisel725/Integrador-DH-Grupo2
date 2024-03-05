b8d3f1bc7012097731e9259dc765e7dc2fe61801
Sonia Olivera <61550104+SoniaOlivera@users.noreply.github.com>
2024-03-05 16:36:57 -0300

    Update baseDeDatos.sql
    Se agrega usuario y tipo de usuario

--- BaseDeDatos/baseDeDatos.sql
+++ BaseDeDatos/baseDeDatos.sql
@@ -1,29 +1,45 @@
 --Creación de productos y categorías
-CREATE TABLE IF NOT EXISTS Categoria (
+CREATE TABLE IF NOT EXISTS CATEGORIA (
     ID_Categoria SERIAL PRIMARY KEY,
     Titulo VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Producto (
+CREATE TABLE IF NOT EXISTS PRODUCTO (
     ID_producto SERIAL PRIMARY KEY,
     Nombre VARCHAR(255) NOT NULL,
     Descripcion VARCHAR(255),
-	ID_Categoria INTEGER NOT NULL,
-    CONSTRAINT fk_Categoria
+	ID_Categoria INTEGER,
+    CONSTRAINT fk_CATEGORIA
         FOREIGN KEY(ID_Categoria) 
-        REFERENCES Categoria(ID_Categoria)
+        REFERENCES CATEGORIA(ID_Categoria)
         ON DELETE CASCADE
 );
 
 
-CREATE TABLE IF NOT EXISTS imagen (
+CREATE TABLE IF NOT EXISTS IMAGEN (
     ID_imagen SERIAL PRIMARY KEY,
     ID_producto INTEGER NOT NULL,
     titulo VARCHAR(255),
     urlimg VARCHAR(255),
-    CONSTRAINT fk_producto
+    CONSTRAINT fk_PRODUCTO
         FOREIGN KEY(ID_producto) 
-        REFERENCES producto(ID_producto)
+        REFERENCES PRODUCTO(ID_producto)
         ON DELETE CASCADE
 );
+--Creación de Usuario y Tipo de Usuario
+CREATE TABLE IF NOT EXISTS tipo_usuario (
+    ID_tipo_usuario SERIAL PRIMARY KEY,
+    Tipo VARCHAR(255) NOT NULL
+);
 
+CREATE TABLE IF NOT EXISTS USUARIO (
+    ID_usuario SERIAL PRIMARY KEY,
+    Nombre VARCHAR(255) NOT NULL,
+    Apellido VARCHAR(255) NOT NULL,
+    Mail VARCHAR(255) NOT NULL,
+	ID_tipo_usuario INTEGER,
+    CONSTRAINT fk_USUARIO
+        FOREIGN KEY(ID_tipo_usuario) 
+        REFERENCES CATEGORIA(ID_tipo_usuario)
+        ON DELETE CASCADE
+);
